553633ad20862fde1c7420af4b96c534e93ce1a3
Sultan Omar <90979782+Sultanomar0013@users.noreply.github.com>
2025-04-10 13:17:41 +0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-
-import './App.css'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import LogIn from './pages/LogIn';
+import Home from './pages/home';
 
 function App() {
+    return (
+        <Router>
 
 
-  return (
-    <>
+                <Routes>
+                    <Route path="/" element={<LogIn />} />
+                    <Route path="/messages" element={<Home />} />
+                </Routes>
 
-    </>
-  )
+        </Router>
+    );
 }
 
-export default App
+export default App;
